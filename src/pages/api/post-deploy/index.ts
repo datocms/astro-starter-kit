d2f419267fe7c6a551e1134f4921fe268b3af5e6
Stefano Verna <s.verna@datocms.com>
2026-01-30 13:18:39 +0100

    Update content link attribute and add visual editing config
    Rename data-datocms-content-link-group to data-datocms-content-link-boundary
    and add visualEditing settings to the web previews plugin installation.

--- src/pages/api/post-deploy/index.ts
+++ src/pages/api/post-deploy/index.ts
@@ -31,6 +31,13 @@ async function installWebPreviewsPlugin(client: Client, baseUrl: string) {
             `/api/preview-links?token=${SECRET_API_TOKEN}`,
             baseUrl,
           ).toString(),
+          visualEditing: {
+            enableDraftModeUrl: new URL(
+              `/api/draft-mode/enable?token=${SECRET_API_TOKEN}`,
+              baseUrl,
+            ).toString(),
+            initialPath: '/',
+          },
         },
       ],
       startOpen: true,
